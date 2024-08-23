f2c264ccbb27dc5a2e57bc365ef0fb52193eaaa9
Naaamsukh <naaamsukh@gmail.com>
2024-08-24 02:06:04 +0530

    chore: add exception block in main function

--- app.py
+++ app.py
@@ -17,57 +17,60 @@ OPENAI_API_TOKEN = os.getenv('OPENAI_API_KEY')
 
 
 def main():
-    print("Starting the Infi PDF AI app...")
-    st.title("Infi PDF AI")
+    try:
+        print("Starting the Infi PDF AI app...")
+        st.title("Infi PDF AI")
 
-    # Streamlit sidebar for file upload and processing
-    configure_sidebar()
+        # Streamlit sidebar for file upload and processing
+        configure_sidebar()
 
-    # Main area for chat interface
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+        # Main area for chat interface
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
 
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
-    prompt = st.chat_input("How can I help you?", key="chat_input")
-    if prompt:
-        print("prompt: ", prompt)
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        st.session_state.messages.append({"role": "user", "content": prompt})
-
-        # If the retrieval chain is created, use it to answer the user's question
-        if "query_engine" in st.session_state:
-            chunks = st.session_state.query_engine.query(prompt)
-
-            context_str = "\n\n".join([chunk.get("content") for chunk in chunks])
-            
-            prompt = GENERAL_RAG_PROMPT.format(question=prompt, context=context_str)
+        prompt = st.chat_input("How can I help you?", key="chat_input")
+        if prompt:
             print("prompt: ", prompt)
-            response = call_openai_api(prompt, DEFAULT_SYSTEM_PROMPT, model="gpt-4o")
-            
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
-            sources = "["
+            # If the retrieval chain is created, use it to answer the user's question
+            if "query_engine" in st.session_state:
+                chunks = st.session_state.query_engine.query(prompt)
 
-            # Extracting the sources of the retrieved documents
-            for i, chunk in enumerate(chunks):
-                metadata = chunk.get("metadata")
-                source = metadata.get("filename")
-                page = metadata.get("page_number")
-                if i == len(chunks) - 1:
-                    sources += f'{source}, Page: {page}'
-                else:
-                    sources += f'{source}, Page: {page} | '
+                context_str = "\n\n".join([chunk.get("content") for chunk in chunks])
+                
+                prompt = GENERAL_RAG_PROMPT.format(question=prompt, context=context_str)
+                print("prompt: ", prompt)
+                response = call_openai_api(prompt, DEFAULT_SYSTEM_PROMPT, model="gpt-4o")
+                
 
-            sources += "]"
+                sources = "["
 
-            # Display the response and sources
-            answer = response + "\n\n*Sources:*\n" + sources
-            st.session_state.messages.append({"role": "assistant", "content": answer})
-            with st.chat_message("assistant"):
-                st.markdown(answer)
+                # Extracting the sources of the retrieved documents
+                for i, chunk in enumerate(chunks):
+                    metadata = chunk.get("metadata")
+                    source = metadata.get("filename")
+                    page = metadata.get("page_number")
+                    if i == len(chunks) - 1:
+                        sources += f'{source}, Page: {page}'
+                    else:
+                        sources += f'{source}, Page: {page} | '
+
+                sources += "]"
+
+                # Display the response and sources
+                answer = response + "\n\n*Sources:*\n" + sources
+                st.session_state.messages.append({"role": "assistant", "content": answer})
+                with st.chat_message("assistant"):
+                    st.markdown(answer)
+    except Exception as e:
+        print("Error occured :",e)
 
 if __name__ == "__main__":
     main()
